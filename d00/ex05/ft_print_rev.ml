666b2a8d5249fa528ac3fa3d061a6e7e022fdceb
Leo SIMON <lsimon@e1r11p17.42.fr>
2019-10-12 16:55:06 +0200

    ft_print_rev: ok

---
+++ d00/ex05/ft_print_rev.ml
@@ -0,0 +1,23 @@
+(* Allowed functions: print_char, String.get, String.length *)
+
+let ft_print_rev s =
+  let rec loop s i =
+    print_char (String.get s i);
+    if i > 0 then
+      loop s (i - 1)
+  in
+  loop s (String.length(s) - 1);
+  print_char '\n'
+
+let test s = 
+  print_string "[Test ";
+  print_string s;
+  print_string "]: ";
+  ft_print_rev s
+
+let main () =
+  test "Hello, world!";
+  test "Open the gates!";
+  test "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  
+let () = main ()
