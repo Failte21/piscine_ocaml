f515bd35f23467c31202ab1676c4a8685556349e
Leo SIMON <lsimon@e1r11p17.42.fr>
2019-10-12 13:27:40 +0200

    initial commit

---
+++ d00/ex00/ft_test_sign.ml
@@ -0,0 +1,19 @@
+let ft_test_sign (number) =
+  if number >= 0
+  then print_endline "positive"
+  else print_endline "negative"
+
+let test (number) =
+  begin
+    print_string "[test ";
+    print_int number;
+    print_string "]: ";
+    ft_test_sign(number);
+  end
+
+let main () =
+  test(42);
+  test(0);
+  test(-1)
+
+let () = main ()
