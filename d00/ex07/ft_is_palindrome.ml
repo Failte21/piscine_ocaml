925664faf7f44d5fd8981139fc0948d2294d39d7
Leo SIMON <lsimon@e1r11p17.42.fr>
2019-10-12 18:09:00 +0200

    ft_is_alindrome: ok

---
+++ d00/ex07/ft_is_palindrome.ml
@@ -0,0 +1,36 @@
+(* Allowed functions: String.get, String.length *)
+
+let is_eq s i =
+  let a = String.get s i in
+  let b = String.get s ((String.length s) - i - 1) in
+  a = b
+
+let ft_is_palindrome s =
+  let rec loop s i =
+    if i > 0 then
+      loop s (i - 1) && is_eq s i
+    else
+      is_eq s i
+  in
+  let len = String.length s in
+  if len = 0 then
+    true
+  else
+    loop s (len - 1)
+
+let test s =
+  print_string "Test \"";
+  print_string s;
+  print_string "\": ";
+  if ft_is_palindrome s then
+    print_string "True\n"
+  else
+    print_string "False\n"
+
+let main () =
+  test "radar";
+  test "car";
+  test "madam";
+  test ""
+  
+let () = main ()
