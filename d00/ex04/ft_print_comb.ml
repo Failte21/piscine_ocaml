be3266040c7af231008624cf1617e12e6a300113
Leo SIMON <lsimon@e1r11p17.42.fr>
2019-10-12 16:07:05 +0200

    ft_print_comb: dumb version

---
+++ d00/ex04/ft_print_comb.ml
@@ -0,0 +1,32 @@
+let ft_print_comb () =
+  let rec loop a b c =
+    print_int a;
+    print_int b;
+    print_int c;
+    if a < 7 then
+      begin
+        print_string ", ";
+        if c = 9 then
+          if b = (c - 1) then
+            begin
+              let next_a = a + 1 in
+              loop next_a b c;
+            end
+          else
+            let next_b = b + 1 in
+            let next_c = next_b + 1 in
+            loop a next_b next_c
+        else
+          let next_c = c + 1 in
+          loop a b next_c
+      end
+    in
+  loop 0 1 2;
+  print_string "\n"
+
+let main () = ft_print_comb()
+  
+let () = main ()
+
+(* allowed: print_string *)
+(* allowed: print_int *)
