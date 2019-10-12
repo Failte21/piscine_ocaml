04fb86e61abd178a9571dff3aa85388770d95368
Leo SIMON <lsimon@e1r11p17.42.fr>
2019-10-12 13:48:48 +0200

    ex01: reccursive version

---
+++ d00/ex01/ft_countdown.ml
@@ -0,0 +1,28 @@
+let rec ft_countdown (number) =
+  if number <= 0
+  then
+    begin
+      print_int 0;
+      print_char '\n'
+    end
+  else
+    begin
+      print_int number;
+      print_char '\n';
+      ft_countdown(number - 1)
+    end
+
+let test (arg) =
+  begin
+    print_string "[test ";
+    print_int arg;
+    print_endline "]:";
+    ft_countdown(arg)
+  end
+
+let main () =
+  test(12);
+  test(5);
+  test(-15)
+
+let () = main ()
