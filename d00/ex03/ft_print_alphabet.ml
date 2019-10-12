be894a1ab542dd7486e33e4d959be2999e00a96d
Leo SIMON <lsimon@e1r11p17.42.fr>
2019-10-12 15:29:17 +0200

    ft_print_alphabet: ok

---
+++ d00/ex03/ft_print_alphabet.ml
@@ -0,0 +1,16 @@
+let ft_print_alphabet () =
+  let rec loop letter =
+    print_char letter;
+    if letter != 'z' then
+      begin
+        let int_letter = int_of_char letter in
+        let next_letter = char_of_int (int_letter + 1) in
+        loop next_letter
+      end
+    in
+    loop 'a';
+    print_char '\n'
+
+let main () = ft_print_alphabet()
+  
+let () = main ()
