a2921bb878d407a75b92b79fc4c34d5350d73592
Leo SIMON <lsimon@e1r4p9.42.fr>
2019-10-14 18:41:40 +0200

    ft_sum: with more tests

--- d01/ex08/ft_sum.ml
+++ d01/ex08/ft_sum.ml
@@ -8,6 +8,29 @@ let ft_sum f i upper_bound =
     ft_sum_aux f (i + 1) upper_bound (f (i + 1) +. acc) in
   ft_sum_aux f i upper_bound (f i)
 
-let main () = print_float (ft_sum (fun i -> float_of_int (i * i)) 1 10)
-  
+
+let test f x n =
+  print_string "Test with ";
+  print_string "[";
+  print_int x;
+  print_char ' ';
+  print_int n;
+  print_string "]: ";
+  print_float (ft_sum f x n);
+  print_char '\n'
+
+let main () =
+  print_endline "With (fun float_of_int(x -> x * x))";
+  test (fun x -> float_of_int(x * x)) 1 10;
+  test (fun x -> float_of_int(x * x)) 1 100;
+  test (fun x -> float_of_int(x * x)) 3 0;
+  test (fun x -> float_of_int(x * x)) 100 (-1);
+  test (fun x -> float_of_int(x * x)) (-3) 3;
+  print_endline "\nWith (fun float_of_int(x -> x * 2))";
+  test (fun x -> float_of_int(x * 2)) 2 4;
+  test (fun x -> float_of_int(x * 2)) 5 3;
+  test (fun x -> float_of_int(x * 2)) 5 0;
+  test (fun x -> float_of_int(x * 2)) 5 (-1);
+  test (fun x -> float_of_int(x * 2)) (-5) 2
+
 let () = main ()
