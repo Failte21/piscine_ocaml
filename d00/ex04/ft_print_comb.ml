88b0d13a95e32fb9675bb11110f4c3ace665c97d
Leo SIMON <lsimon@e2r9p12.42.fr>
2019-10-14 09:35:05 +0200

    ft_print_comb: fixed and cleaned

--- d00/ex04/ft_print_comb.ml
+++ d00/ex04/ft_print_comb.ml
@@ -8,19 +8,10 @@ let ft_print_comb () =
     if a < 7 then
       begin
         print_string ", ";
-        if c = 9 then
-          if b = (c - 1) then
-            begin
-              let next_a = a + 1 in
-              loop next_a b c;
-            end
-          else
-            let next_b = b + 1 in
-            let next_c = next_b + 1 in
-            loop a next_b next_c
-        else
-          let next_c = c + 1 in
-          loop a b next_c
+        let new_a = if b = 8 then a + 1 else a in
+        let new_b = if b = 8 then new_a + 1 else if c == 9 then b + 1 else b in
+        let new_c = if c = 9 then new_b + 1 else c + 1 in
+        loop new_a new_b new_c
       end
     in
   loop 0 1 2;
