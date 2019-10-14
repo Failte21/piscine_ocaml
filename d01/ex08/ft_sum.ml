22294b3adaeee76ae51b4a677097e156937f9ef4
Leo SIMON <lsimon@e1r4p9.42.fr>
2019-10-14 18:34:49 +0200

    ft_sum: acc version

--- d01/ex08/ft_sum.ml
+++ d01/ex08/ft_sum.ml
@@ -1,10 +1,12 @@
 (* Allowed functions : float_of_int *)
 (* If the upper bound is less than the lower bound, ft_sum must return nan. *)
 
-let rec ft_sum f i upper_bound =
+let ft_sum f i upper_bound =
   if upper_bound < i then nan else
-  if upper_bound = i then f i else
-  f i +. (ft_sum f (i + 1) upper_bound)
+  let rec ft_sum_aux f i upper_bound acc =
+    if upper_bound = i then acc else
+    ft_sum_aux f (i + 1) upper_bound (f (i + 1) +. acc) in
+  ft_sum_aux f i upper_bound (f i)
 
 let main () = print_float (ft_sum (fun i -> float_of_int (i * i)) 1 10)
   
