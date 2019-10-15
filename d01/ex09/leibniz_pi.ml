c9145e5dac83d53f862cfcafc8652183487d00aa
Leo SIMON <lsimon@e2r9p14.42.fr>
2019-10-15 09:43:08 +0200

    leibniz_pi: fair tests

--- d01/ex09/leibniz_pi.ml
+++ d01/ex09/leibniz_pi.ml
@@ -3,20 +3,33 @@
 (* If the given delta is negative, your
 function will return -1 *)
 
+(* Its type will be float -> int *)
+
 let leibniz_pi delta =
   if delta < 0.0 then -1 else
   let f = (fun n -> ((-1.0) ** (float_of_int n)) /. ((2.0 *. (float_of_int n)) +. 1.0)) in
-  let pi = float_of_int 4 *. atan 1.0 in
+  let pi_ref = float_of_int 4 *. atan 1.0 in
   let rec loop i acc =
-    print_float (4.0 *. acc);
-    print_char '\n';
     let result = (f i) +. acc in
-    let diff = pi -. (4.0 *. result) in
+    let diff = pi_ref -. (4.0 *. result) in
     let diff_abs = if diff > 0.0 then diff else -.diff in
     if diff_abs < delta then i
     else loop (i + 1) (result) in
   loop 0 0.0
 
-let main () = print_int (leibniz_pi 0.001)
+let test delta =
+  print_string "Test with ";
+  print_string "[";
+  print_float delta;
+  print_string "]: ";
+  print_int (leibniz_pi delta);
+  print_char '\n'
+
+let main () =
+  test 1.0;
+  test 0.1;
+  test (-1.0);
+  test 0.000001;
+  test 0.000000001
   
 let () = main ()
