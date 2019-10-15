33349636ab790429393bc3099808ef6e05f691c7
Leo SIMON <lsimon@e2r9p14.42.fr>
2019-10-15 09:29:22 +0200

    leibniz: getting somewhere

--- d01/ex09/leibniz_pi.ml
+++ d01/ex09/leibniz_pi.ml
@@ -5,15 +5,19 @@ function will return -1 *)
 
 let leibniz_pi delta =
   if delta < 0.0 then -1 else
-  let f = (fun n -> ((float_of_int (-1)) ** (float_of_int n)) /. (float_of_int 2 *. float_of_int n +. 1.0)) in
+  let f = (fun n -> ((-1.0) ** (float_of_int n)) /. ((2.0 *. (float_of_int n)) +. 1.0)) in
   let pi = float_of_int 4 *. atan 1.0 in
-  let rec loop i delta acc =
-    let result = (f i) +. acc in
-    let diff = pi -. (4.0 *. result) in
-    let diff_abs = if diff > 0.0 then diff else -.diff in
-    if diff_abs < delta then i
-    else loop (i + 1) delta (result) in
-  loop 1 delta 0.0
+  let rec loop i acc =
+    print_float (4.0 *. acc);
+    print_char '\n';
+    if i == 1000 then i
+    else
+      let result = (f i) +. acc in
+      (* let diff = pi -. (4.0 *. result) in
+      let diff_abs = if diff > 0.0 then diff else -.diff in
+      if diff_abs < delta then i *)
+      loop (i + 1) (result) in
+  loop 0 0.0
 
 let main () = print_int (leibniz_pi 3.14)
   
