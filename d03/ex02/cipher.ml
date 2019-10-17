3bf4c943f335c8ea9af5e2be04a2add18aacd89c
Leo SIMON <lsimon@e2r8p16.42.fr>
2019-10-17 14:37:54 +0200

    encrypt: seems okay

--- d03/ex02/cipher.ml
+++ d03/ex02/cipher.ml
@@ -1,28 +1,38 @@
 (* Allowed functions : Pervasives module and the String.map function *)
 
-let ascii_rot n c a_int =
-  let c_int = int_of_char c in
-  let base = c_int - a_int + n in
-  let result = base mod 26 in
-  char_of_int (a_int + result)
-
 let rot n c =
-  match c with
-  | 'a' .. 'z' -> ascii_rot n c (int_of_char 'a')
-  | 'A' .. 'Z' -> ascii_rot n c (int_of_char 'A')
-  | _ -> c
+  let c_int = int_of_char c in
+  char_of_int ((c_int + n) mod 128)
 
 let ceasar s i = String.map (rot i) s
 
-let unceasar s i = String.map (rot (26 - (i mod 26))) s
+let unceasar s i = String.map (rot ((128 - i))) s
 
 let rot42 s = ceasar s 42
 
 let unrot42 s = unceasar s 42
 
+let xorc key c = char_of_int ((int_of_char c) lxor key)
+
+let xor key s = String.map (xorc key) s
+
+let ft_crypt (s: string) fns =
+  let rec ft_crypt_aux fns acc =
+    match fns with
+    | fn::t -> ft_crypt_aux t (fn s)
+    | [] -> acc in
+  ft_crypt_aux fns ""
+
 let main () =
-  let rot_hello = rot42 "hello le zebre 1234" in
+  let rot_hello = ceasar "hello le zebre 1234" 1 in
   print_endline rot_hello;
-  print_endline (unrot42 rot_hello)
+  print_endline (unceasar rot_hello 1);
+  let xored = xor 5 "hello" in
+  print_endline xored;
+  print_endline (xor 5 xored);
+  print_endline "----ABOUT TO ENCRYPT [hello]----";
+  let crypted = ft_crypt "hello" [rot42; (xor 18)] in
+  print_endline crypted;
+  print_endline (ft_crypt crypted [rot42; (xor 18)])
 
 let () = main ()
