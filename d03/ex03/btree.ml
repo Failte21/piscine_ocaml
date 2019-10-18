d0c17415eea56c1e13868865220618cbfd6ea4d1
Leo SIMON <lsimon@e2r8p16.42.fr>
2019-10-18 09:10:40 +0200

    is_balanced: seems ok

--- d03/ex03/btree.ml
+++ d03/ex03/btree.ml
@@ -26,9 +26,30 @@ let is_perfect = function
       | Nil -> true in
   (is_perfect_aux tree) && (is_bst tree)
 
+let is_balanced = function
+| Nil -> true
+| tree ->
+  let rec is_balanced_aux node acc =
+  match node with
+    | Node (_, Nil, Nil) -> true
+    | Node (_, Nil, right) -> acc && (is_balanced_aux right false)
+    | Node (_, left, Nil) -> acc && (is_balanced_aux left false)
+    | Node (_, left, right) -> (is_balanced_aux left acc) && (is_balanced_aux right acc)
+    | Nil -> true in
+  (is_balanced_aux tree true) && (is_bst tree)
+
 let bst_trees = [
   let f = Node (65, Nil, Nil) in
   let e = Node (70, f, Nil) in
+  let d = Node (30, Nil, Nil) in
+  let c = Node (60, Nil, e) in
+  let b = Node (40, d, Nil) in
+  let a = Node (50, b, c) in
+  a
+]
+
+let balanced_trees = [
+  let e = Node (70, Nil, Nil) in
   let d = Node (30, Nil, Nil) in
   let c = Node (60, Nil, e) in
   let b = Node (40, d, Nil) in
@@ -46,6 +67,8 @@ let perfect_trees = [
 let main () =
   List.iter (fun tree -> if (is_bst tree) then print_endline "Tree is bst" else print_endline "Tree is not bst") bst_trees;
   List.iter (fun tree -> if (is_perfect tree) then print_endline "Tree is perfect" else print_endline "Tree is not perfect") bst_trees;
-  List.iter (fun tree -> if (is_perfect tree) then print_endline "Tree is perfect" else print_endline "Tree is not perfect") perfect_trees
+  List.iter (fun tree -> if (is_perfect tree) then print_endline "Tree is perfect" else print_endline "Tree is not perfect") perfect_trees;
+  List.iter (fun tree -> if (is_balanced tree) then print_endline "Tree is balanced" else print_endline "Tree is not balanced") bst_trees;
+  List.iter (fun tree -> if (is_balanced tree) then print_endline "Tree is balanced" else print_endline "Tree is not balanced") balanced_trees
 
 let () = main ()
