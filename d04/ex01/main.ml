8411384962295edbab9de42328943d5002ef3f27
Leo SIMON <lsimon@e2r8p16.42.fr>
2019-10-19 09:33:17 +0200

    Value: fixed

--- d04/ex01/main.ml
+++ d04/ex01/main.ml
@@ -9,23 +9,26 @@ let previous_str v =
   (Value.toString v) ^ "->" ^ (Value.toString (Value.previous v))
 
 let main () =
-  let all_values = Value.all in
+  let without_2 = List.tl Value.all in
+  let without_as = List.rev (List.tl (List.rev Value.all)) in
+  let t_2 = List.hd Value.all in
+  let t_as = List.hd (List.rev Value.all) in
   Printf.printf "%s-----Value.toString-----%s\n" green reset;
   List.iter print_endline (List.map Value.toString Value.all);
   print_char '\n';
   Printf.printf "%s-----Value.toStringVerbose-----%s\n" green reset;
-  List.iter print_endline (List.map Value.toStringVerbose all_values);
+  List.iter print_endline (List.map Value.toStringVerbose Value.all);
   print_char '\n';
-  Printf.printf "%s-----Value.next-----%s\n" green reset;
-  List.iter print_endline (List.map next_str Value.without_as);
+  Printf.printf "%s-----Value.next (without AS)-----%s\n" green reset;
+  List.iter print_endline (List.map next_str without_as);
   print_char '\n';
-  Printf.printf "%s-----Value.previous-----%s\n" green reset;
-  List.iter print_endline (List.map previous_str Value.without_2);
+  Printf.printf "%s-----Value.previous (without 2)-----%s\n" green reset;
+  List.iter print_endline (List.map previous_str without_2);
   print_char '\n';
-  Printf.printf "%s-----Value.next As-----%s\n" green reset;
-  List.iter print_endline (List.map next_str Value.all);
+  Printf.printf "%s-----Value.next AS-----%s\n" green reset;
+  print_endline (next_str t_as);
   print_char '\n';
   Printf.printf "%s-----Value.previous 2-----%s\n" green reset;
-  List.iter print_endline (List.map previous_str Value.all)
+  print_endline (previous_str t_2)
 
 let () = main ()
